4f004db9eecf4eb3e4bad1d76f10b0e9502b3742
Etimbuk <euedofia@gmail.com>
2020-02-20 15:54:58 +0000

    FPLA-1324 add Application.java test

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/adoption/ApplicationTest.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.adoption;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+    @Test
+    public void applicationContextLoaded() {
+    }
+
+    @Test
+    public void applicationContextTest() {
+        Application.main(new String[] {});
+    }
+}
