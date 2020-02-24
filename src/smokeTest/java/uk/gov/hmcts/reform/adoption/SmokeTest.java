60f10554b86a7d0fb1ca668bc52ddd4c417966e6
Etimbuk <euedofia@gmail.com>
2020-02-24 18:48:08 +0000

    FPLA-1325 attempt fixing SSLHandshakeException in pipeline

--- src/smokeTest/java/uk/gov/hmcts/reform/adoption/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/adoption/SmokeTest.java
@@ -1,12 +1,17 @@
 package uk.gov.hmcts.reform.adoption;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import static java.net.http.HttpResponse.BodyHandlers;
 import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
@@ -15,10 +20,32 @@ import static org.springframework.http.HttpStatus.OK;
 
 public class SmokeTest {
     private final String targetInstance =
-        defaultIfBlank(
-            System.getenv("TEST_URL"),
-            "http://localhost:4550"
-        );
+        defaultIfBlank(System.getenv("TEST_URL"), "http://localhost:4550");
+
+    @BeforeEach
+    void setup() throws Exception {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] {
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(
+                    java.security.cert.X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(
+                    java.security.cert.X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+
+        // Install the all-trusting trust manager
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    }
 
     @Test
     public void shouldProveAppIsRunningAndHealthy() throws IOException, InterruptedException {
