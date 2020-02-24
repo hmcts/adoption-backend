6e65e71370c19d60e39d482af1cbd1fe44966d7b
Etimbuk <euedofia@gmail.com>
2020-02-24 16:42:01 +0000

    FPLA-1325 remove serenity and rest-assured

--- src/smokeTest/java/uk/gov/hmcts/reform/adoption/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/adoption/SmokeTest.java
@@ -1,8 +1,14 @@
 package uk.gov.hmcts.reform.adoption;
 
-import io.restassured.RestAssured;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static java.net.http.HttpResponse.*;
 import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.OK;
@@ -15,18 +21,20 @@ public class SmokeTest {
         );
 
     @Test
-    public void shouldProveAppIsRunningAndHealthy() {
-        RestAssured.baseURI = targetInstance;
-        RestAssured.useRelaxedHTTPSValidation();
+    public void shouldProveAppIsRunningAndHealthy() throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newHttpClient();
 
-        String response = RestAssured
-            .get("/health")
-            .then()
-            .assertThat()
-            .statusCode(OK.value())
-            .and()
-            .extract().body().asString();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(targetInstance + "/health"))
+            .build();
 
-        assertThat(response).contains("UP");
+        HttpResponse<String> httpResponse =
+            httpClient.send(request, BodyHandlers.ofString());
+
+        assertThat(httpResponse.statusCode())
+            .isEqualTo(OK.value());
+
+        assertThat(httpResponse.body())
+            .contains("UP");
     }
 }
