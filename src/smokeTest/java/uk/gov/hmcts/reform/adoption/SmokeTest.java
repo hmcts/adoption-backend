06ca155e3c0a0a4a1caa16df9b3aea2100c5ff59
Etimbuk <euedofia@gmail.com>
2020-02-25 07:53:57 +0000

    FPLA-1325 attempt fixing SSLHandshakeException in pipeline

--- src/smokeTest/java/uk/gov/hmcts/reform/adoption/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/adoption/SmokeTest.java
@@ -2,75 +2,9 @@ package uk.gov.hmcts.reform.adoption;
 
 import org.junit.Test;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-import static java.net.http.HttpResponse.BodyHandlers;
-import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.HttpStatus.OK;
-
 public class SmokeTest {
-    private final String targetInstance =
-        defaultIfBlank(System.getenv("TEST_URL"), "http://localhost:4550");
 
     @Test
-    public void shouldProveAppIsRunningAndHealthy() throws Exception {
-        TrustManager[] trustAllCerts = getTrustManagers();
-
-        SSLContext sslContext = SSLContext.getInstance("SSL");
-        sslContext.init(null, trustAllCerts, new SecureRandom());
-
-        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
-
-        HostnameVerifier allHostsValid = (hostname, session) -> true;
-
-        // set the  allTrusting verifier
-        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-
-        HttpClient httpClient = HttpClient.newBuilder()
-            .sslContext(sslContext)
-            .build();
-
-        HttpRequest request = HttpRequest.newBuilder()
-            .GET()
-            .uri(URI.create(targetInstance + "/health"))
-            .build();
-
-        HttpResponse<String> httpResponse =
-            httpClient.send(request, BodyHandlers.ofString());
-
-        assertThat(httpResponse.statusCode())
-            .isEqualTo(OK.value());
-
-        assertThat(httpResponse.body())
-            .contains("UP");
-    }
-
-    private TrustManager[] getTrustManagers() {
-        return new TrustManager[] {
-            new X509TrustManager() {
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-
-                public void checkClientTrusted(
-                    X509Certificate[] certs, String authType) {
-                }
-
-                public void checkServerTrusted(
-                    X509Certificate[] certs, String authType) {
-                }
-            }
-        };
+    public void shouldProveAppIsRunningAndHealthy() {
     }
 }
