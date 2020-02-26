5a534ca43e01420cb0ef98df0f6221372cc8740d
Etimbuk U <Gogetter@users.noreply.github.com>
2020-02-26 15:59:12 +0000

    fix: include smoke test directory (#13)
    * FPLA-1325 terraform updates
    
    * FPLA-1325 terraform updates
    
    * FPLA-1325 terraform updates
    
    * FPLA-1325 terraform updates
    
    * FPLA-1325 include smoke test directory
    
    * FPLA-1325 include smoke test directory
    
    * FPLA-1325 use RestAssured and included suppressions

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/adoption/SmokeTest.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.adoption;
+
+import io.restassured.RestAssured;
+import org.junit.Test;
+
+import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.OK;
+
+public class SmokeTest {
+    private final String targetInstance = defaultIfBlank(System.getenv("URL"), "http://localhost:4550");
+
+    @Test
+    public void shouldProveAppIsRunningAndHealthy() {
+        RestAssured.baseURI = targetInstance;
+        RestAssured.useRelaxedHTTPSValidation();
+
+        String response = RestAssured
+            .when()
+            .get("/health")
+            .then()
+            .statusCode(OK.value())
+            .and()
+            .extract().body().asString();
+
+        assertThat(response)
+            .contains("UP");
+    }
+}
