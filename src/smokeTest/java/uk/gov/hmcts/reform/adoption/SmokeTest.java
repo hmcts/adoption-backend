c0ad57fcce40316a67d1bc9c59f249ffc32b94cf
Etimbuk <euedofia@gmail.com>
2020-02-26 15:49:51 +0000

    FPLA-1325 use RestAssured and included suppressions

--- src/smokeTest/java/uk/gov/hmcts/reform/adoption/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/adoption/SmokeTest.java
@@ -1,23 +1,8 @@
 package uk.gov.hmcts.reform.adoption;
 
+import io.restassured.RestAssured;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-import static java.net.http.HttpResponse.BodyHandlers;
 import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.OK;
@@ -26,53 +11,19 @@ public class SmokeTest {
     private final String targetInstance = defaultIfBlank(System.getenv("URL"), "http://localhost:4550");
 
     @Test
-    public void shouldProveAppIsRunningAndHealthy() throws NoSuchAlgorithmException, KeyManagementException,
-        IOException, InterruptedException {
-        TrustManager[] trustAllCerts = getTrustManagers();
+    public void shouldProveAppIsRunningAndHealthy() {
+        RestAssured.baseURI = targetInstance;
+        RestAssured.useRelaxedHTTPSValidation();
 
-        SSLContext sslContext = SSLContext.getInstance("SSL");
-        sslContext.init(null, trustAllCerts, new SecureRandom());
+        String response = RestAssured
+            .when()
+            .get("/health")
+            .then()
+            .statusCode(OK.value())
+            .and()
+            .extract().body().asString();
 
-        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
-
-        HostnameVerifier allHostsValid = (hostname, session) -> true;
-
-        // set the  allTrusting verifier
-        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-
-        HttpClient httpClient = HttpClient.newBuilder()
-            .sslContext(sslContext)
-            .build();
-
-        HttpRequest request = HttpRequest.newBuilder()
-            .GET()
-            .uri(URI.create(targetInstance + "/health"))
-            .build();
-
-        HttpResponse<String> httpResponse = httpClient.send(request, BodyHandlers.ofString());
-
-        assertThat(httpResponse.statusCode())
-            .isEqualTo(OK.value());
-
-        assertThat(httpResponse.body())
+        assertThat(response)
             .contains("UP");
-    }
-
-    private TrustManager[] getTrustManagers() {
-        return new TrustManager[] {
-            new X509TrustManager() {
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-
-                public void checkClientTrusted(
-                    X509Certificate[] certs, String authType) {
-                }
-
-                public void checkServerTrusted(
-                    X509Certificate[] certs, String authType) {
-                }
-            }
-        };
     }
 }
