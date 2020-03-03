64745b672799c0fee2181b273c7c77db448b0a09
Sam Walker <sam.walker@hmcts.net>
2020-03-03 17:07:31 +0000

    correct spelling of microservicekey

--- infrastructure/s2s_config.tf
+++ infrastructure/s2s_config.tf
@@ -3,12 +3,12 @@ data "azurerm_key_vault" "s2s_vault" {
   resource_group_name = "rpe-service-auth-provider-${var.env}"
 }
 
-data "azurerm_key_vault_secret" "source_microservice-adoption-backend" {
+data "azurerm_key_vault_secret" "source_microservicekey-adoption-backend" {
   name         = "microservicekey-adoption-backened"
   key_vault_id = "${data.azurerm_key_vault.s2s_vault.id}"
 }
 
-resource "azurerm_key_vault_secret" "microservice-adoption-backend" {
+resource "azurerm_key_vault_secret" "microservicekey-adoption-backend" {
   name         = "microservicekey-adoption-backend"
   value        = "${data.azurerm_key_vault_secret.source_microservicekey-adoption-backend.value}"
   key_vault_id = "${module.key-vault.key_vault_id}"
