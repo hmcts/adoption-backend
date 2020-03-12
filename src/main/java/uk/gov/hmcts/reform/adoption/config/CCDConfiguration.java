67ba0a04e9e3d44457ee5da0fc9797fda023ff22
Alex McAusland <alex.mcausland@gmail.com>
2020-03-12 09:21:40 +0000

    Add complex

--- src/main/java/uk/gov/hmcts/reform/adoption/config/CCDConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/adoption/config/CCDConfiguration.java
@@ -4,6 +4,7 @@ import uk.gov.hmcts.ccd.sdk.types.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.types.ConfigBuilder;
 import uk.gov.hmcts.reform.adoption.enums.State;
 import uk.gov.hmcts.reform.adoption.enums.UserRole;
+import uk.gov.hmcts.reform.adoption.models.Applicant;
 import uk.gov.hmcts.reform.adoption.models.CaseData;
 
 public class CCDConfiguration implements CCDConfig<CaseData, State, UserRole> {
@@ -19,7 +20,8 @@ public class CCDConfiguration implements CCDConfig<CaseData, State, UserRole> {
             .grant("CRU", UserRole.Citizen)
             .grant("R", UserRole.AdoptionClerk)
             .fields()
-            .optional(CaseData::getApplicant)
-            .mandatory(CaseData::getFoo);
+            .mandatory(CaseData::getFoo)
+            .complex(CaseData::getApplicant)
+                .mandatory(Applicant::getName);
     }
 }
